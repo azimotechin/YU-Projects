07e6a8ea9b8f8d12c8971b0980a00b367c9f62dd
azimotechin <azimotechin@gmail.com>
2025-05-23 14:44:20 -0400

    Test

--- DataStructures/project/stage6/src/test/java/edu/yu/cs/com1320/project/impl/BTreeImplTest.java
+++ DataStructures/project/stage6/src/test/java/edu/yu/cs/com1320/project/impl/BTreeImplTest.java
@@ -12,6 +12,7 @@ import edu.yu.cs.com1320.project.stage6.impl.DocumentPersistenceManager;
 import org.junit.jupiter.api.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
@@ -146,5 +147,116 @@ public class BTreeImplTest {
         assertEquals(2, wordCountMap.get("hello"));
         assertEquals(1, wordCountMap.get("world"));
         assertNull(wordCountMap.get("nonexistent"));
+    }
+
+    @Test
+    public void testGetExistingKey() {
+        URI uri = makeUri("doc1");
+        Document doc = makeDoc("doc1", "existing document");
+        bTree.put(uri, doc);
+        Document result = bTree.get(uri);
+        assertNotNull(result);
+        assertEquals("existing document", result.getDocumentTxt());
+    }
+
+    @Test
+    public void testPutNewKey() {
+        URI uri = makeUri("doc2");
+        Document doc = makeDoc("doc2", "new document");
+        bTree.put(uri, doc);
+        Document result = bTree.get(uri);
+        assertNotNull(result);
+        assertEquals("new document", result.getDocumentTxt());
+    }
+
+    @Test
+    public void testNodeSplit() {
+        for (int i = 1; i <= 7; i++) {
+            URI uri = makeUri("doc" + i);
+            Document doc = makeDoc("doc" + i, "document " + i);
+            bTree.put(uri, doc);
+        }
+        assertNotNull(bTree.get(makeUri("doc1")));
+        assertNotNull(bTree.get(makeUri("doc7")));
+    }
+
+    @Test
+    public void testMoveToDisk() throws IOException {
+        URI uri = makeUri("doc1");
+        Document doc = makeDoc("doc1", "document to disk");
+        bTree.put(uri, doc);
+        bTree.moveToDisk(uri);
+        File expectedFile = new File(baseDir, "test.com" + File.separator + "doc1.json");
+        assertTrue(expectedFile.exists(), "Expected serialized file to exist at: " + expectedFile.getPath());
+    }
+
+    @Test
+    public void testDeserializationOnNullValue() throws Exception {
+        URI uri = makeUri("doc1");
+        Document doc = makeDoc("doc1", "text");
+        bTree.put(uri, doc);
+        bTree.moveToDisk(uri);
+        Document result = bTree.get(uri);
+        assertEquals("text", result.getDocumentTxt());
+    }
+
+    @Test
+    public void testPutReplacesValueAndDeletesFromPersistence() {
+        URI uri = makeUri("doc2");
+        Document doc1 = makeDoc("doc2", "first");
+        Document doc2 = makeDoc("doc2", "second");
+        bTree.put(uri, doc1);
+        Document old = bTree.put(uri, doc2);
+        assertNotNull(old, "Old document should not be null");
+        assertEquals("first", old.getDocumentTxt());
+        assertEquals("second", bTree.get(uri).getDocumentTxt());
+    }
+
+    @Test
+    public void testPutReturnsNullOnNoSplit() {
+        URI uri = makeUri("nosplit");
+        Document doc = makeDoc("nosplit", "data");
+        assertNull(bTree.put(uri, doc));
+    }
+
+    @Test
+    public void testPutTriggersSplitAndNewRoot() {
+        for (int i = 1; i <= 10; i++) {
+            URI uri = makeUri("doc" + i);
+            Document doc = makeDoc("doc" + i, "text " + i);
+            bTree.put(uri, doc);
+        }
+        for (int i = 1; i <= 10; i++) {
+            URI uri = makeUri("doc" + i);
+            assertNotNull(bTree.get(uri));
+        }
+    }
+
+    @Test
+    public void testPutChildBranchingLogic() {
+        URI uri1 = makeUri("a");
+        URI uri2 = makeUri("z");
+        Document doc1 = makeDoc("a", "alpha");
+        Document doc2 = makeDoc("z", "zeta");
+        bTree.put(uri1, doc1);
+        bTree.put(uri2, doc2);
+        assertEquals("alpha", bTree.get(uri1).getDocumentTxt());
+        assertEquals("zeta", bTree.get(uri2).getDocumentTxt());
+    }
+
+    @Test
+    public void testMoveToDiskSkipsNullValue() throws Exception {
+        URI uri = makeUri("skip");
+        bTree.moveToDisk(uri);
+    }
+
+    @Test
+    public void testValueSetToNullAfterMoveToDisk() throws Exception {
+        URI uri = makeUri("doc3");
+        Document doc = makeDoc("doc3", "nullify me");
+        bTree.put(uri, doc);
+        bTree.moveToDisk(uri);
+        Document result = bTree.get(uri);
+        assertEquals("nullify me", result.getDocumentTxt());
     }
 }
